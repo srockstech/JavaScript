ee3c4f2d3640f8b4fd9c4350713dd00963b4c57b
Sarvagya Verma <sarvagya1115@gmail.com>
2023-01-25 00:02:19 +0530

    Added function.js and removed methods.js

--- functions.js
+++ functions.js
@@ -12,3 +12,6 @@ var p1 = new person("Sarvagya Verma", 22);
 
 console.log(year());
 
+//ES6 syntax
+const greet = v => "Welcome " + v; //For a one line function returning a value, we can exempt from using return keyword and braces
+console.log(greet("Sarvagya"));
