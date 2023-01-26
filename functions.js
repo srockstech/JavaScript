345d0ec8e03253e623a1d9eca1fb55d01dab2d2b
Sarvagya Verma <sarvagya1115@gmail.com>
2023-01-26 23:59:43 +0530

    Modified functions.js

--- functions.js
+++ functions.js
@@ -13,5 +13,17 @@ var p1 = new person("Sarvagya Verma", 22);
 console.log(year());
 
 //ES6 syntax
+//Functions can be defined like this, when there are no parameters
+const greet1 = () => {
+    console.log("Hello there!");
+}
+
+const greet2 = x => {
+    console.log("Hello " + x);
+}
+
 const greet = v => "Welcome " + v; //For a one line function returning a value, we can exempt from using return keyword and braces
+
+greet1();
+
 console.log(greet("Sarvagya"));
