56e6aa946c6707dd081d49c353bf7409a1bd1360
Sarvagya Verma <sarvagya1115@gmail.com>
2023-02-13 23:59:34 +0530

    Added notes to dom.js

--- dom.js
+++ dom.js
@@ -1,5 +1,15 @@
+//to fetch firstElementChild or lastElementChild
 var heading = document.firstElementChild.lastElementChild.firstElementChild;
 console.log(heading);
+
+//Modifying HTML text of an element
 heading.innerHTML = "Good!";
+
+//To search the first occurance of an particular tag
 document.querySelector("h1").style.color = "blue";
-document.getElementsByTagName("li")[2].style.color = "red"; //3rd element's color property set to red. getElementsByTagName returns a list of all the elements avaialable in the document
+
+//To get a list of all the elements of a particular tag and modifying their properties.
+document.getElementsByTagName("li")[2].style.color = "green"; //3rd element's color property set to red. getElementsByTagName returns a list of all the elements avaialable in the document
+console.log(document.getElementsByClassName("btn").length);
+document.querySelector("li a").style.color = "orange";
+document.getElementsByTagName("li")[1].style.color = "green";
